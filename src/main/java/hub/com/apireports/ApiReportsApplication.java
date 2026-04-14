498cccf0c00dc33faf6e6e172cb01d39d4878fbc
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-04-14 11:06:03 -0500

    empty

---
+++ src/main/java/hub/com/apireports/ApiReportsApplication.java
@@ -0,0 +1,13 @@
+package hub.com.apireports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiReportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hub/com/apireports/ApiReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package hub.com.apireports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
