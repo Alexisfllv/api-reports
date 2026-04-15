e2f53d9d32b87c8035f3b2980d924ead5988712a
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-04-14 21:40:09 -0500

    feat(security): #9 AuthController implement service Post register,login and Get findAllMembers

---
+++ src/main/java/hub/com/apireports/security/AuthController.java
@@ -0,0 +1,42 @@
+package hub.com.apireports.security;
+
+
+import hub.com.apireports.dto.security.AuthResponse;
+import hub.com.apireports.dto.security.LoginRequest;
+import hub.com.apireports.dto.security.MemberDTOResponse;
+import hub.com.apireports.dto.security.RegisterRequest;
+import hub.com.apireports.service.security.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+// #9
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService  authService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> registerPost(@RequestBody RegisterRequest registerRequest){
+        AuthResponse res =  authService.register(registerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> loginPost(@RequestBody LoginRequest loginRequest){
+        AuthResponse res =  authService.login(loginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+
+    @GetMapping("/findAll")
+    public ResponseEntity<List<MemberDTOResponse>> findAllGet(){
+        List<MemberDTOResponse> res =  authService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(res);
+    }
+}
