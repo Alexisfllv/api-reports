d8351b9cc7612c2a8d5b0ff910285e8c0024aad9
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-04-16 08:49:52 -0500

    feat(category): Category add service GET getAllCategories with Unit Test (#7)
    * feat(category): Category add service GET getAllCategories
    
    * test(category): CategoryServiceTest add service test GET getAllCategories

--- src/main/java/hub/com/apireports/controller/CategoryController.java
+++ src/main/java/hub/com/apireports/controller/CategoryController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +22,11 @@ public class CategoryController {
             @Valid @RequestBody CategoryDTORequest categoryDTORequest){
         CategoryDTOResponse res = categoryService.createCategory(categoryDTORequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(res);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTOResponse>> getAllCategories(){
+        List<CategoryDTOResponse> res = categoryService.getAllCategories();
+        return ResponseEntity.status(HttpStatus.OK).body(res);
     }
 }
--- src/test/java/hub/com/apireports/service/impl/CategoryServiceTest.java
+++ src/test/java/hub/com/apireports/service/impl/CategoryServiceTest.java
@@ -8,6 +8,7 @@ import hub.com.apireports.mapper.CategoryMapper;
 import hub.com.apireports.repo.CategoryRepo;
 import hub.com.apireports.service.CategoryService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
@@ -15,6 +16,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -65,5 +69,48 @@ public class CategoryServiceTest {
         inOrder.verify(categoryRepo).save(category);
         inOrder.verify(categoryMapper).toCategoryDTOResponse(category);
         inOrder.verifyNoMoreInteractions();
+    }
+
+    @Nested
+    class getAllCategories {
+        @Test
+        public void getAllCategories_whenExist_returnList() {
+            // Arrange
+            List<Category> categoryList = List.of(category);
+            List<CategoryDTOResponse> categoryDTOResponseList = List.of(categoryDTOResponse);
+             when(categoryRepo.findAll()).thenReturn(categoryList);
+             when(categoryMapper.toCategoryDTOResponse(category)).thenReturn(categoryDTOResponse);
+            // Act
+            List<CategoryDTOResponse> result = categoryService.getAllCategories();
+            // Assert
+            assertAll(
+                    () -> assertEquals(categoryDTOResponseList.size(), result.size()),
+                    () -> assertEquals(categoryDTOResponseList.get(0).id(), result.get(0).id()),
+                    () -> assertEquals(categoryDTOResponseList.get(0).name(), result.get(0).name()),
+                    () -> assertEquals(categoryDTOResponseList.get(0).description(), result.get(0).description()),
+                    () -> assertEquals(categoryDTOResponseList.get(0).active(), result.get(0).active())
+            );
+
+            // InOrder & Verify
+            InOrder inOrder = Mockito.inOrder(categoryRepo, categoryMapper);
+            inOrder.verify(categoryRepo).findAll();
+            inOrder.verify(categoryMapper).toCategoryDTOResponse(category);
+            inOrder.verifyNoMoreInteractions();
+        }
+
+        @Test
+        public void getAllCategories_whenNotExist_returnEmptyList() {
+            // Arrange
+            List<Category> emptyList = List.of();
+            when(categoryRepo.findAll()).thenReturn(emptyList);
+            // Act
+            List<CategoryDTOResponse> result = categoryService.getAllCategories();
+            // Assert
+            assertEquals(0, result.size());
+            // InOrder & Verify
+            InOrder inOrder = Mockito.inOrder(categoryRepo, categoryMapper);
+            inOrder.verify(categoryRepo).findAll();
+            inOrder.verifyNoMoreInteractions();
+        }
     }
 }
