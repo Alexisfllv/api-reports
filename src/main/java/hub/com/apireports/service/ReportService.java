cd8f7cee66a7cc5ac310e476b32c3b69e6c4f53a
AlexisFl <159475523+Alexisfllv@users.noreply.github.com>
2026-05-01 20:42:55 -0500

    feat(report): Report add service GET getAllReports

--- src/main/java/hub/com/apireports/service/ReportService.java
+++ src/main/java/hub/com/apireports/service/ReportService.java
@@ -3,8 +3,14 @@ package hub.com.apireports.service;
 import hub.com.apireports.dto.report.ReportDTORequest;
 import hub.com.apireports.dto.report.ReportDTOResponse;
 
+import java.util.List;
+
 public interface ReportService {
 
     // POST
     ReportDTOResponse createReport(ReportDTORequest reportDTORequest,Long memberId);
+
+
+    // GET
+    List<ReportDTOResponse> getAllReports();
 }
